f9fc65222c7f0c5e666da4d21a2f904d72282582
Noqmar <noqmar@mintshell.org>
2017-12-11 14:36:06 +0100

    updated file disclaimers

--- mintshell-core/src/test/java/org/mintshell/command/parameter/PrimitiveParameterTest.java
+++ mintshell-core/src/test/java/org/mintshell/command/parameter/PrimitiveParameterTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2017 mintshell.org
+ * Copyright © 2017-2018 mintshell.org
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
